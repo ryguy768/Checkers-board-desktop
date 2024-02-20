4a922460a4b3261532c6aa03e4affc31b9e65721
ryguy768 <67657968+ryguy768@users.noreply.github.com>
2024-02-20 17:31:22 -0500

    Put the board into js

--- board.js
+++ board.js
@@ -1,23 +1,23 @@
 $(document).ready(function () {
-    console.log("document ready");
-    $(".red.cell, .blue.cell").click(toggle);
-  });
-  
-  function toggle() {
-    let checker = $(this).children().first();
-    checker.toggle();
-    if (!checker.is(":visible")) {
-      switchColor(checker);
-    }
+  console.log("document ready");
+  $('#board-container').html(boardTemplate())
+  $(".red.cell, .blue.cell").click(toggle);
+});
+
+function toggle() {
+  let checker = $(this).children().first();
+  checker.toggle();
+  if (!checker.is(":visible")) {
+    switchColor(checker);
   }
-  
-  function switchColor(checker) {
-    if (checker.hasClass("red-checker")) {
-      checker.removeClass("red-checker");
-      checker.addClass("blue-checker");
-    } else {
-      checker.addClass("red-checker");
-      checker.removeClass("blue-checker");
-    }
+}
+
+function switchColor(checker) {
+  if (checker.hasClass("red-checker")) {
+    checker.removeClass("red-checker");
+    checker.addClass("blue-checker");
+  } else {
+    checker.addClass("red-checker");
+    checker.removeClass("blue-checker");
   }
-  
+}
---
+++ template.js
@@ -0,0 +1,41 @@
+function cellTemplate(rowNum, cellNum) {
+    var isCellEven = true
+    var isRowEven = true
+    if (cellNum % 2 == 1) isCellEven = false
+    if (rowNum % 2 == 1) isRowEven = false
+
+    if (isCellEven != isRowEven) {
+        // Black Cell
+        let cellString = `<div id="cell-${rowNum}-${cellNum}" class="cell red">`
+        if (rowNum <= 3) {
+            cellString = cellString + `<div class="checker blue-checker"></div>`
+        } else if (rowNum >= 6) {
+            cellString = cellString + `<div class="checker red-checker"></div>`
+        } else {
+            cellString = cellString + `<div class="checker red-checker" hidden="true"></div>`
+        }
+        cellString = cellString + `</div>`
+        return cellString
+    } else
+        // White Cell
+        return `<div id="cell-${rowNum}-${cellNum}" class="cell blue"></div>`
+}
+
+function rowTemplate(rowNum) {
+    console.log(`drawing row number ${rowNum}`)
+    var rowString = ''
+    rowString = rowString + `<div id="row-${rowNum}" class="row">`
+    for (var cellNum=1; cellNum<=8; cellNum++) {
+        rowString = rowString + cellTemplate(rowNum, cellNum)
+    }
+    rowString = rowString + `</div>`
+    return rowString
+}
+
+function boardTemplate() {
+    var boardString = ''
+    for (var rowNum=1; rowNum<=8; rowNum++) {
+        boardString = boardString + rowTemplate(rowNum)
+    }
+    return boardString
+}
