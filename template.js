b7bc59098e5ee47748b450962596c9ee9e6296b1
ryguy768 <67657968+ryguy768@users.noreply.github.com>
2024-02-21 13:32:18 -0500

    Update template.js

--- template.js
+++ template.js
@@ -1,56 +1,59 @@
-function renderCell(rowNum, cellNum) {
-  if (determineColor(rowNum, cellNum) === "blue") {
-    // determine the cells color
-    // Red Cell
-    let cellString = `<div id="cell-${rowNum}-${cellNum}" class="cell red">`;
-    if (rowNum <= 3) {
-      cellString = cellString + renderChecker("blue");
-    } else if (rowNum >= 6) {
-      cellString = cellString + renderChecker("red");
-    } else {
-      cellString = cellString + renderChecker("red", "hidden");
-    }
-    cellString = cellString + `</div>`;
-    return cellString;
-  }
-  // Blue Cell
-  else return `<div id="cell-${rowNum}-${cellNum}" class="cell blue"></div>`;
-}
-function renderChecker(color, hidden) {
-  return `<div class="checker ${color}-checker" ${hidden}></div>`;
-}
-function parity(num) {
-  if (num % 2 === 0) return "even";
-  else return "odd";
-}
-
-function determineColor(rowNum, cellNum) {
-  if (parity(rowNum) === parity(cellNum)) return "red";
-  else return "blue";
+function renderBoard() {
+  return `
+    ${renderRow(1)}
+    ${renderRow(2)}
+    ${renderRow(3)}
+    ${renderRow(4)}
+    ${renderRow(5)}
+    ${renderRow(6)}
+    ${renderRow(7)}
+    ${renderRow(8)}
+  `;
 }
 
 function renderRow(rowNum) {
-  console.log(`drawing row number ${rowNum}`);
-  var rowString = "";
-  rowString = rowString + `<div id="row-${rowNum}" class="row">`;
-  for (var cellNum = 1; cellNum <= 8; cellNum++) {
-    rowString = rowString + renderCell(rowNum, cellNum);
-  }
-  rowString = rowString + `</div>`;
-  return rowString;
+  return `
+    <div id="row-${rowNum}" class="row">
+      ${renderCell(rowNum, 1)}
+      ${renderCell(rowNum, 2)}
+      ${renderCell(rowNum, 3)}
+      ${renderCell(rowNum, 4)}
+      ${renderCell(rowNum, 5)}
+      ${renderCell(rowNum, 6)}
+      ${renderCell(rowNum, 7)}
+      ${renderCell(rowNum, 8)}
+    </div>
+  `;
 }
 
-function renderBoard() {
-  return `
- ${renderRow(1)}
- ${renderRow(2)}
- ${renderRow(3)}
- ${renderRow(4)}
- ${renderRow(5)}
- ${renderRow(6)}
- ${renderRow(7)}
- ${renderRow(8)}
- 
-  `
+function renderCell(rowNum, cellNum) {
+  if (determineColor(rowNum, cellNum) === "red") {
+    // Red cell
+    return `
+      <div id="cell-${rowNum}-${cellNum}" class="cell red">
+        ${renderChecker(rowNum)}
+      </div>
+    `;
+  } else {
+    // Blue cell
+    return `<div id="cell-${rowNum}-${cellNum}" class="cell blue"></div>`;
+  }
+}
 
+function renderChecker(rowNum) {
+  if (rowNum <= 3) {
+    return '<div class="checker blue-checker"></div>';
+  } else if (rowNum >= 6) {
+    return `<div class="checker red-checker"></div>`;
+  } else {
+    return `<div class="checker red-checker" hidden="true"></div>`;
+  }
+}
+
+function parity(num) {
+  return (num % 2 === 0) ? "even" : "odd";
+}
+
+function determineColor(rowNum, cellNum) {
+  return parity(rowNum) == parity(cellNum) ? "red" : "blue";
 }
